75705f0d120f57e1d08bf1af08f754cf1d1ea18a
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-05-20 07:50:49 -0300

    exemplo de uso dos métodos de arquivos

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 void main() {
 
-    File file = new File("arquivo.txt");
+    File file = new File("data/arquivo.txt");
 
     if(!file.exists()){
         try{
+            File parent = new File(file.getAbsoluteFile().getParent());
+            parent.mkdir();
             file.createNewFile();
         }catch (IOException ex){
             System.out.println("Falha ao criar");
         }
     }
+
+    System.out.println("Tamanho: "+file.length());
+    System.out.println("Diretório pai: "+file.getAbsoluteFile().getParent());
+    System.out.println("Caminho: "+file.getAbsoluteFile().getPath());
 
 }
