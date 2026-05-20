d5cb33ac6fd047eb25fb0f4b2bd2ad3ff4f1d0d4
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-05-20 08:24:19 -0300

    Escrevendo lista em arquivo de texto

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.Pessoa;
+
 void main() {
 
     File file = new File("data/Pessoas.txt");
@@ -12,12 +14,22 @@ void main() {
         }
     }
 
-    //Jeito desorganizado...
+    List<Pessoa> pessoas = List.of(
+            new Pessoa("111.111.111-01", "João"),
+            new Pessoa("222.222.222-02", "Maria"),
+            new Pessoa("333.333.333-03", "Pedro")
+    );
+
+    //Jeito desorganizado de fazer a escrita...
     try(BufferedWriter writer = new BufferedWriter(
             new FileWriter(file, true)
     )){
-        writer.write("Hello World");
-        writer.newLine();
+        for(Pessoa pessoa : pessoas){
+            writer.write(pessoa.getCpf());
+            writer.write(';');
+            writer.write(pessoa.getNome());
+            writer.newLine();
+        }
     } catch (IOException e) {
         System.out.println("Falha ao escrever no arquivo");
     }
