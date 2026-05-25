57068055ee03f1f5959781c465c2994cb7eb4633
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-05-25 08:37:31 -0300

    Testando os métodos

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,10 @@ void main() {
     }
 
     try{
+        Pessoa pessoa = new Pessoa("111.111.111-01",
+                "João da Silva",
+                LocalDate.of(2000,3,10));
+        genericDao.atualizar(pessoa);
         System.out.println(genericDao.listar());
     } catch (IOException e) {
         System.out.println("Falha ao abrir arquivo");
