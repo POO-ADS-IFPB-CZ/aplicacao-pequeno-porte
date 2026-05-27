41c1e58926b53ec04f4bb86d0e97c95300ea2e38
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-05-27 08:42:17 -0300

    exemplo com show confirm dialog

--- src/view/Seletor.java
+++ src/view/Seletor.java
@@ -8,11 +8,21 @@ public class Seletor {
         ImageIcon icon = new ImageIcon(
                 "src/imgs/error.png"
         );
-        JOptionPane.showMessageDialog(null,
-                "Hello world",
-                "Título da janela",
-                JOptionPane.ERROR_MESSAGE,
-                icon);
+        int retorno = JOptionPane.showConfirmDialog(null,
+                "A operação pode ocasionar perda de dados",
+                "Selecione uma opção",
+                JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.WARNING_MESSAGE);
+
+        switch (retorno){
+            case JOptionPane.CANCEL_OPTION ->
+                    System.out.println("Cancelou");
+            case JOptionPane.OK_OPTION ->
+                    System.out.println("OK");
+            case JOptionPane.CLOSED_OPTION ->
+                    System.out.println("Fechou");
+        }
+
     }
 
 }
