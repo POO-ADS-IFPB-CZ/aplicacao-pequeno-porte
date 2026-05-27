562bcb2cb0ad4b23c208615e6257042955d50837
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-05-27 08:17:52 -0300

    Exemplo de showmessagedialog

--- src/view/Seletor.java
+++ src/view/Seletor.java
@@ -1,23 +1,18 @@
 package view;
 
 import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Seletor {
 
     static void main() {
-        JFileChooser chooser = new JFileChooser();
-        chooser.setDialogTitle("Escolher o arquivo");
-        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        chooser.setFileFilter(new FileNameExtensionFilter(
-                "Arquivos de imagem",
-                "PNG","JPEG", "GIF"));
-        chooser.showDialog(null, "Abrir");
-
-        if(chooser.getSelectedFile() == null){
-            System.out.println("Não escolheu nada...");
-        }
-
+        ImageIcon icon = new ImageIcon(
+                "src/imgs/error.png"
+        );
+        JOptionPane.showMessageDialog(null,
+                "Hello world",
+                "Título da janela",
+                JOptionPane.ERROR_MESSAGE,
+                icon);
     }
 
 }
